55f25017ba148463be4cb73a44a34a6b9d0885f4
Victor Querino Martins <victor.querino.martins@gmail.com>
2026-04-01 16:04:58 -0300

    feat: add lógica do input

--- pokedex/script.js
+++ pokedex/script.js
@@ -1,5 +1,7 @@
 const URL = "https://pokeapi.co/api/v2/pokemon?limit=151";
 
+let todosPokemons = [];
+
 async function chamarAPI() {
   const resposta = await fetch(URL);
   if (resposta.status === 200) {
@@ -20,11 +22,41 @@ async function chamarAPI() {
         >
         <p class="pokemon-name">${dadosPokemon.name}</p>
       </div>`;
+
+      todosPokemons.push(dadosPokemon);
+
       document.querySelector("#pokemon-grid").innerHTML += cardHTML;
     }
   }
 }
 
-document.querySelector("#pokemon-grid").innerHTML;
+input = document.querySelector(".search-input");
+
+input.addEventListener("input", function () {
+  const valorInput = input.value.toLowerCase();
+
+  const pokemonsFiltrados = todosPokemons.filter((pokemon) => pokemon.name.includes(valorInput),);
+
+  document.querySelector("#pokemon-grid").innerHTML = ""; // Limpa o grid antes de exibir os result filtrados
+
+  for (let i = 0; i < pokemonsFiltrados.length; i++) {
+    const dadosPokemon = pokemonsFiltrados[i];
+
+    const cardHTML = `<div class="pokemon-card">
+        <div class="card-top">
+          <span class="pokemon-type ${dadosPokemon.types[0].type.name}">${dadosPokemon.types[0].type.name}</span>
+          <span class="pokemon-number">${dadosPokemon.order}</span>
+        </div>
+        <img
+          src="${dadosPokemon.sprites.front_default}"
+          alt="${dadosPokemon.name}"
+          class="pokemon-img"
+        >
+        <p class="pokemon-name">${dadosPokemon.name}</p>
+      </div>`;
+
+    document.querySelector("#pokemon-grid").innerHTML += cardHTML;
+  }
+});
 
 chamarAPI();
