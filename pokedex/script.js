0a600d4535c3bc7affa3ce277c946d94ce87c53c
Victor Querino Martins <victor.querino.martins@gmail.com>
2026-04-02 17:41:34 -0300

    feat: logica da pagincao e funcao desenhar

--- pokedex/script.js
+++ pokedex/script.js
@@ -1,46 +1,19 @@
 const URL = "https://pokeapi.co/api/v2/pokemon?limit=151";
 
 let todosPokemons = [];
+let paginaAtual = 1;
+const itensPorPagina = 24;
 
-async function chamarAPI() {
-  const resposta = await fetch(URL);
-  if (resposta.status === 200) {
-    const obj = await resposta.json(); // transformar em objeto
+function desenharTela(listaDeDados) {
+  let pginicio = (paginaAtual - 1) * itensPorPagina;
+  let pgfim = paginaAtual * itensPorPagina;
+  const pokemonsDaPagina = listaDeDados.slice(pginicio, pgfim);
 
-    for (let i = 0; i < obj.results.length; i++) {
-      const respostaD = await fetch(obj.results[i].url);
-      const dadosPokemon = await respostaD.json();
-      const cardHTML = `<div class="pokemon-card">
-        <div class="card-top">
-          <span class="pokemon-type ${dadosPokemon.types[0].type.name}">${dadosPokemon.types[0].type.name}</span>
-          <span class="pokemon-number">${dadosPokemon.order}</span>
-        </div>
-        <img
-          src="${dadosPokemon.sprites.front_default}"
-          alt="${dadosPokemon.name}"
-          class="pokemon-img"
-        >
-        <p class="pokemon-name">${dadosPokemon.name}</p>
-      </div>`;
+  const grid = document.querySelector("#pokemon-grid");
+  grid.innerHTML = "";
 
-      todosPokemons.push(dadosPokemon);
-
-      document.querySelector("#pokemon-grid").innerHTML += cardHTML;
-    }
-  }
-}
-
-input = document.querySelector(".search-input");
-
-input.addEventListener("input", function () {
-  const valorInput = input.value.toLowerCase();
-
-  const pokemonsFiltrados = todosPokemons.filter((pokemon) => pokemon.name.includes(valorInput),);
-
-  document.querySelector("#pokemon-grid").innerHTML = ""; // Limpa o grid antes de exibir os result filtrados
-
-  for (let i = 0; i < pokemonsFiltrados.length; i++) {
-    const dadosPokemon = pokemonsFiltrados[i];
+  for (let i = 0; i < pokemonsDaPagina.length; i++) {
+    const dadosPokemon = pokemonsDaPagina[i];
 
     const cardHTML = `<div class="pokemon-card">
         <div class="card-top">
@@ -55,7 +28,47 @@ input.addEventListener("input", function () {
         <p class="pokemon-name">${dadosPokemon.name}</p>
       </div>`;
 
-    document.querySelector("#pokemon-grid").innerHTML += cardHTML;
+    grid.innerHTML += cardHTML;
+  }
+}
+
+async function chamarAPI() {
+  const resposta = await fetch(URL);
+  if (resposta.status === 200) {
+    const obj = await resposta.json();
+
+    for (let i = 0; i < obj.results.length; i++) {
+      const respostaD = await fetch(obj.results[i].url);
+      const dadosPokemon = await respostaD.json(); // dados do pokemon específico
+      todosPokemons.push(dadosPokemon);
+    }
+
+    desenharTela(todosPokemons);
+  }
+}
+
+const input = document.querySelector(".search-input"); // input para pesquisa
+
+input.addEventListener("input", function () {
+  const valorInput = input.value.toLowerCase();
+  const pokemonsFiltrados = todosPokemons.filter((pokemon) =>
+    pokemon.name.includes(valorInput),
+  );
+
+  paginaAtual = 1;
+
+  desenharTela(pokemonsFiltrados); // desenha os pokemons filtrados
+});
+
+document.querySelector(".page-next").addEventListener("click", function () {
+  paginaAtual++;
+  desenharTela(todosPokemons);
+});
+
+document.querySelector(".page-prev").addEventListener("click", function () {
+  if (paginaAtual > 1) {
+    paginaAtual--;
+    desenharTela(todosPokemons);
   }
 });
 
